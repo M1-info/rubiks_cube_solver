4d322568f4df0aef2f30ed1c9f67564a34662cc0
ChrisTom-94 <christian.tomasino.dev@gmail.com>
2023-03-18 14:53:15 +0100

    include glfw and opengl

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,20 @@
-let () = print_endline "Hello, World!"
+open Tgl4
+
+let () =
+  (* Initialize the library *)
+  GLFW.init ();
+  at_exit GLFW.terminate;
+  (* Create a windowed mode window and its OpenGL context *)
+  let window = GLFW.createWindow ~width:640 ~height:480 ~title:"Hello World" () in
+  (* Make the window's context current *)
+  GLFW.makeContextCurrent ~window:(Some window);
+  (* Loop until the user closes the window *)
+  while not (GLFW.windowShouldClose ~window:window) do
+    (* Render here *)
+    Gl.clear_color 1. 0. 0. 1.;
+    Gl.clear Gl.color_buffer_bit;
+    (* Swap front and back buffers *)
+    GLFW.swapBuffers ~window:window;
+    (* Poll for and process events *)
+    GLFW.pollEvents ()
+  done
